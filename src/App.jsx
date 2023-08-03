653cb7f97303f6d260f10fa0ea985e4465620b11
Mostak Khan <Mostak1993@gmail.com>
2023-08-03 06:26:34 +0100

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,259 @@
 import { useState, useEffect } from "react";
-import {  Tabs, TabsHeader, TabsBody, Tab, TabPanel,} from "@material-tailwind/react"
 import axios from "axios";
 import "../App.css";
 
+//Search Component
+const SearchPokemon = ({
+  searchQuery,
+  setSearchQuery,
+  searchType,
+  setSearchType,
+  searchSection,
+  setSearchSection,
+}) => {
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchTypeChange = (e) => {
+    setSearchType(e.target.value);
+  };
+
+  const handleSearchSectionChange = (e) => {
+    setSearchSection(e.target.value);
+  };
+
+  return (
+    <div className="text-center">
+      <form action="">
+        <div>
+          <label>
+            <input
+              type="radio"
+              value="name"
+              checked={searchSection === "name"}
+              onChange={handleSearchSectionChange}
+              className="mx-3 px-3"
+            />{" "}
+            Search by Pokemon Name
+          </label>{" "}
+          <label>
+            <input
+              type="radio"
+              value="type"
+              checked={searchSection === "type"}
+              onChange={handleSearchSectionChange}
+              className="mx-3 px-3"
+            />{" "}
+            Search by Pokemon Type
+          </label>
+        </div>
+
+        {searchSection === "name" && (
+          <>
+            Search Pokemon Name:{" "}
+            <input
+              className="my-3 mx-3 px-2 outline outline-2"
+              type="text"
+              value={searchQuery}
+              onChange={handleSearchChange}
+            />
+          </>
+        )}
+
+        {searchSection === "type" && (
+          <>
+            Search Pokemon Type:{" "}
+            <input
+              className="my-3 mx-3 px-2 outline outline-2"
+              type="text"
+              value={searchType}
+              onChange={handleSearchTypeChange}
+            />
+          </>
+        )}
+      </form>
+    </div>
+  );
+};
+
+//App Component
 function App() {
   /*
   This code initializes a state variable called 'pokemon' and a function called 'setPokemon' 
   to update the state. The 'useStates' hook from React is used to create this state variable and
   its initial value is set to an empty array '[]'
   */
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchType, setSearchType] = useState("");
+  const [searchSection, setSearchSection] = useState("name");
+
+  const typeColors = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "#C22E28",
+    poison: "#A33EA1",
+    ground: "#E2BF65",
+    flying: "#A98FF3",
+    psychic: "#F95587",
+    bug: "#A6B91A",
+    rock: "#B6A136",
+    ghost: "#735797",
+    dragon: "#6F35FC",
+    dark: "#705746",
+    steel: "#B7B7CE",
+    fairy: "#D685AD",
+  };
+
   /* 
   This code uses the 'useEffect' hook from React to fetch data from an external API
   when the component mounts or updates
   */
-  useEffect(() => {
-  /*  */
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
-      .then((res) => {
-        const pokemonPromises = res.data.results.map((result) =>
-          axios.get(result.url)
+  const fetchData = async () => {
+    try {
+      const pokemonResponse = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=302"
+      );
+      const pokemonData = pokemonResponse.data.results;
+
+      const allPokemonData = [];
+
+      for (const result of pokemonData) {
+        const pokemonResponse = await axios.get(result.url);
+        const speciesResponse = await axios.get(
+          result.url.replace("pokemon", "pokemon-species")
         );
-        axios
-          .all(pokemonPromises)
-          .then((pokemon) => setPokemon(pokemon))
-          .catch((error) => console.error(error));
-      })
-      .catch((error) => console.error(error));
+
+        const pokemon = pokemonResponse.data;
+        const species = speciesResponse.data;
+
+        allPokemonData.push({ ...pokemon, speciesData: species });
+      }
+      console.log(allPokemonData);
+      setPokemonData(allPokemonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
   }, []);
+
+  const filteredPokemon =
+    pokemonData.length > 0
+      ? pokemonData.filter((pokemon) => {
+          const nameMatch = pokemon.name
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase());
+          const typeMatch = searchType
+            ? pokemon.types.some(
+                (type) =>
+                  type.type.name.toLowerCase() === searchType.toLowerCase()
+              )
+            : true;
+          return searchSection === "name" ? nameMatch : typeMatch;
+        })
+      : [];
 
   return (
     <div>
-      <h1 className="text-center mt-2 text-[32pt] font-bold">Pokemon: Gotta Catch Em All</h1>
+      <h1 className="text-center mt-2 text-[32pt] font-bold">
+        Pokemon: Gotta Catch Em All
+      </h1>
 
-      <div className="flex flex-wrap justify-center ">
-        {pokemon.map((pokemon) => (
-          <div
-            className="max-w-sm m-[20px] rounded-b-lg overflow-hidden border-[5px] border-black shadow-[0_35px_60px_10px_rgba(0,0,0,0.6)] h-[400px] w-[350px]"
-            key={pokemon.data.id}
-          >
-            <h1 className="font-bold text-[18pt] mb-2 mt-4 text-center">
-              {pokemon.data.name}
-            </h1>
-            <div className="text-center">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                Types(s):{" "}
-                <strong>
-                  {pokemon.data.types.map((type) => type.type.name).join(" / ")}{" "}
-                </strong>
-              </span>
-            </div>
-            <div className="flex justify-center h-[150px]">
-              <img
-                src={pokemon.data.sprites.other.dream_world.front_default}
-                alt={pokemon.data.name}
-              />
-            </div>
-            <div className="text-center p-5">
-            <span className="inline-block bg-gray-200 mt-1 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                Moves(s):{" "}
-                <strong>
-                  {pokemon.data.abilities.map((abilities) => abilities.ability.name).join(" / ")}{" "}
-                </strong>
-              </span>
+      <SearchPokemon
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        searchType={searchType}
+        setSearchType={setSearchType}
+        searchSection={searchSection}
+        setSearchSection={setSearchSection}
+      />
+      <div className="flex flex-wrap justify-center">
+        {filteredPokemon.length === 0 ? (
+          <p> No pokemon Found, please try a different name! </p>
+        ) : (
+          filteredPokemon.map((pokemon) => (
+            <div
+              className="flex group h-[467px] w-[350px] max-w-sm m-[20px] overflow-hidden shadow-[0_1px_10px_0_rgba(0,0,0,0.8)]"
+              key={pokemon.id}
+            >
+              <div className="flex-col h-full w-full bg-red-300 transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+                <div className="relative top-[10px] text-center">
+                  <h1 className="text-[20pt] font-bold">{pokemon.name}</h1>
 
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                Moves(s):{" "}
-                <strong>
-                  {pokemon.data.moves.slice(0, 4).map((move) => move.move.name).join(" / ")}{" "}
-                </strong>
-              </span>
+                  {/*Images */}
+                  <div className="flex relative justify-center top-[20px] h-[300px]">
+                    <img
+                      src={pokemon.sprites.other.dream_world.front_default}
+                      alt={pokemon.name}
+                    />
+                  </div>
+
+                  {/* Pokemon Types */}
+                  <span className="inline-block relative top-[50px] text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Types(s):{" "}
+                    {pokemon.types.map((type) => (
+                      <div
+                        key={type.type.name}
+                        style={{
+                          backgroundColor: typeColors[type.type.name],
+                        }}
+                        className="px-3 mx-1 inline-block rounded-[5%] text-white"
+                      >
+                        <div className="">{type.type.name}</div>
+                      </div>
+                    ))}
+                  </span>
+                </div>
+
+                <div className="absolute inset-0 h-[467px] w-[350px] bg-green-600 text-center [transform:rotateY(180deg)] [backface-visibility:hidden]">
+                  <div className="flex-col absolute right-0 left-0 top-[50px]">
+                    <div className="h-[100px] m-3">
+                      <p className="font-bold">Description:</p>
+
+                      {pokemon.speciesData.flavor_text_entries[9].language
+                        .name === "en" && (
+                        <li
+                          className="list-none text-justify"
+                          key={pokemon.speciesData.version}
+                        >
+                          {
+                            pokemon.speciesData.flavor_text_entries[9]
+                              .flavor_text
+                          }
+                        </li>
+                      )}
+                    </div>
+                    {/* Abilities */}
+                    <span className="text-[12pt] font-semibold text-black">
+                      Abilities:
+                      <div className="gap-6 font-bold">
+                        {pokemon.abilities
+                          .map((abilities) => abilities.ability.name)
+                          .join(" | ")}
+                      </div>
+                    </span>
+                    <br />
+                    {/* Stats */}
+                    <span className="text-[12pt] font-semibold text-black">
+                      {pokemon.stats[0].stat.name.toUpperCase()}: {pokemon.stats[0].base_stat}
+                    </span>
+                    <span>
+                      
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
