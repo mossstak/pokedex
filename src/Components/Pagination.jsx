b21633627be0d7c9feb8c3d18d31b0b5e879a11b
Mostak Khan <Mostak1993@gmail.com>
2023-09-06 23:56:01 +0100

    made some changes

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -3,8 +3,22 @@ import React from "react";
 const Pagination = ({ gotoNextPage, gotoPrevPage }) => {
   return (
     <div className="flex gap-4 justify-center">
-      {gotoPrevPage && <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={gotoPrevPage}>Previous</button>}
-      {gotoNextPage && <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={gotoNextPage}>Next</button>}
+      {gotoPrevPage && (
+        <button
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          onClick={gotoPrevPage}
+        >
+          Previous
+        </button>
+      )}
+      {gotoNextPage && (
+        <button
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          onClick={gotoNextPage}
+        >
+          Next
+        </button>
+      )}
     </div>
   );
 };
