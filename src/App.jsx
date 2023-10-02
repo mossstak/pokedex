0f521accf07d6d669cd2d4f71457d22dac7a2c08
Mostak Khan <Mostak1993@gmail.com>
2023-10-02 15:53:14 +0100

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,76 @@
 /* eslint-disable no-unused-vars */
 import { React, useEffect, useState } from "react";
 import { PokemonCards } from "./Components/PokemonCards";
+import { SearchPokemon } from "./Components/SearchPokemon";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [searchPokemon, setSearchPokemon] = useState("");
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchPokeData();
   }, []);
 
-  const fetchPokeData = () => {
+  const fetchPokeData = async () => {
     let endpoints = [];
     for (let i = 1; i < 151; i++) {
       endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
     }
-    let response = axios
-      .all(endpoints.map((endpoint) => axios.get(endpoint)))
-      .then((res) => {
-        console.log(res);
-        setLoading(true);
-        setPokemon(res);
-      });
-    return response;
+
+    try {
+      let res = await axios.all(
+        endpoints.map((endpoint) => axios.get(endpoint))
+      );
+      setPokemon(res);
+      setFilteredPokemon(res);
+      setLoading(true);
+      console.log(res);
+    } catch (error) {
+      console.error("Failed to fetch pokemons:", error);
+    }
   };
+
+  const handleSearch = () => {
+    if (searchPokemon === "") {
+      setFilteredPokemon(pokemon);
+    } else {
+      setFilteredPokemon(
+        pokemon.filter((poke) =>
+          poke.data.name.toLowerCase().includes(searchPokemon.toLowerCase())
+        )
+      );
+    }
+  };
+
+  const resetPokemon = () => {
+    setFilteredPokemon(pokemon);
+  }
 
   return (
     <div className="container m-auto p-3">
       {loading ? (
         <div>
-          <h1 className="text-[32pt] font-bold text-center">Pokedex: Gotta Catch Em All</h1>
+          <h1 className="text-[32pt] font-bold text-center">
+            Pokedex: Gotta Catch Em All
+          </h1>
+          <div className="flex gap-3 justify-center px-5">
+            <SearchPokemon
+              placeholder="search Pokemon Here"
+              onChange={(e) => setSearchPokemon(e.target.value)}
+              value={searchPokemon}
+              onClick={handleSearch}
+            />
+            <button className="p-3 w-[100px] hover:bg-red-800 hover:text-white hover:transition: border border-red-600" onClick={resetPokemon}>
+              Reset
+            </button>
+          </div>
           <div className="p-3 grid-cols-3 sm:grid md:grid-cols-3">
-            {pokemon.map((pokemon) => (
+            {filteredPokemon.map((pokemon) => (
               <PokemonCards
                 key={uuidv4()}
                 name={pokemon.data.name}
---
+++ src/Components/SearchPokemon.jsx
@@ -0,0 +1,26 @@
+import React, {useState } from "react";
+import "../App.css";
+
+export const SearchPokemon = ({ value, onChange, onClick, placeholder }) => {
+  const [inputPlaceholder, setInputPlaceholder] = useState(placeholder);
+
+  return (
+    <div className="flex gap-3 justify-center">
+      <input
+        className="md:w-[500px] h-[50px] border border-red-600"
+        type="text"
+        placeholder={inputPlaceholder}
+        value={value}
+        onChange={onChange}
+        onFocus={() => setInputPlaceholder("")}
+        onBlur={() => setInputPlaceholder(placeholder)}
+      />
+      <button
+        className="p-3 w-[100px] hover:bg-red-800 hover:text-white hover:transition: border border-red-600"
+        onClick={onClick}
+      >
+        Search
+      </button>
+    </div>
+  );
+};
